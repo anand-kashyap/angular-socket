7127317d796edb69f6a8a96d4a1835f3813af525
Anand Kashyap <ak@cloudify.biz>
2019-07-30 09:45:14 +0530

    add socket io basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as io from 'socket.io-client';
+import { environment } from '../environments/environment';
+import { Observable, Observer } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-socket';
+  socket: SocketIOClient.Socket;
+  socketUrl = environment.socketUrl;
+  count: string;
+  messages = [];
+  chatContent = '';
+  constructor() {
+    this.socket = io.connect(this.socketUrl);
+  }
+
+  ngOnInit() {
+    this.onNewMessage().subscribe(message => {
+      console.log(message);
+      this.messages.push(message);
+    });
+  }
+
+  sendMessage(msg) {
+    this.socket.emit('newMessage', msg.value);
+  }
+
+  onNewMessage() {
+    return new Observable((observer: Observer<any>) => {
+      this.socket.on('newMessage', msg => {
+        observer.next(msg);
+      });
+    });
+  }
 }
