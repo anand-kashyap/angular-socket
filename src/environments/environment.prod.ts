64cf405fdb9db545a299dc97cde86ae55aef3a56
Anand Kashyap <anandkashyap60@gmail.com>
2019-12-23 19:22:09 +0530

    check on diff servers

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   hmr: false,
-  socketUrl: 'https://angular-socket-pwa.herokuapp.com',
-  baseUrl: 'https://angular-socket-pwa.herokuapp.com'
+  socketUrl: 'https://pwa-angular-ak.herokuapp.com',
+  baseUrl: 'https://pwa-angular-ak.herokuapp.com'
 };
