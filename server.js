eb81f8fd1678cbce2bf3c484669dcaba169006b8
Anand Kashyap <anandkashyap60@gmail.com>
2019-12-23 20:29:36 +0530

    add https redirect

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 //Install express server
 const express = require('express'),
-compression = require('compression'),
-path = require('path');
+  compression = require('compression'),
+  enforce = require('express-sslify'),
+  path = require('path');
 
 const app = express();
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(compression());
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/angular-socket'));
 
-app.get('/*', function(req,res) {
+app.get('/*', function (req, res) {
 
-res.sendFile(path.join(__dirname,'/dist/angular-socket/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/angular-socket/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
