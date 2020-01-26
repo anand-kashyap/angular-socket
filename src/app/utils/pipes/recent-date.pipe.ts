ea70f573256be91c71c546eadf4e9fc202f82f04
Anand Kashyap <anandkashyap60@gmail.com>
2020-01-26 16:29:10 +0530

    fix time and color of arrows

--- src/app/utils/pipes/recent-date.pipe.ts
+++ src/app/utils/pipes/recent-date.pipe.ts
@@ -4,23 +4,21 @@ import { formatDate } from '@angular/common';
   name: 'recentDate'
 })
 export class RecentDatePipe implements PipeTransform {
-  timeOnly(date) {
-    console.log('date is', date);
-
-    return formatDate(new Date(date), 'shortTime', 'en');
-  }
   transform(value: any, recentChats = false): any {
     const msgDate = new Date(value).getTime();
     const currentDate = new Date().getTime();
     const bal = (currentDate - msgDate) / (1000 * 60 * 60 * 24); // get diff in days
     if (bal < 1) {
       if (recentChats) {
-        return this.timeOnly(value);
+        return formatDate(new Date(value), 'shortTime', 'en');
       }
       return 'Today';
     } else if (bal < 2) {
       return 'Yesterday';
     } else {
+      if (recentChats) {
+        return formatDate(new Date(value), 'd/M/yy', 'en');
+      }
       return value;
     }
   }
