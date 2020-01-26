325773b190db8b80b4a2153c6e1e80291752aebb
Anand Kashyap <anandkashyap60@gmail.com>
2020-01-26 22:10:19 +0530

    fix recent Screen Date

--- src/app/utils/pipes/recent-date.pipe.ts
+++ src/app/utils/pipes/recent-date.pipe.ts
@@ -4,21 +4,30 @@ import { formatDate } from '@angular/common';
   name: 'recentDate'
 })
 export class RecentDatePipe implements PipeTransform {
+  forRecentScreen(value) {
+    const date = new Date(value);
+    const diff = new Date().getDate() - date.getDate();
+    if (diff === 0) {
+      return formatDate(date, 'shortTime', 'en');
+    } else if (diff === 1) {
+      return 'Yesterday';
+    } else {
+      return formatDate(date, 'd/M/yy', 'en');
+    }
+  }
+
   transform(value: any, recentChats = false): any {
+    if (recentChats) {
+      return this.forRecentScreen(value);
+    }
     const msgDate = new Date(value).getTime();
     const currentDate = new Date().getTime();
     const bal = (currentDate - msgDate) / (1000 * 60 * 60 * 24); // get diff in days
     if (bal < 1) {
-      if (recentChats) {
-        return formatDate(new Date(value), 'shortTime', 'en');
-      }
       return 'Today';
     } else if (bal < 2) {
       return 'Yesterday';
     } else {
-      if (recentChats) {
-        return formatDate(new Date(value), 'd/M/yy', 'en');
-      }
       return value;
     }
   }
