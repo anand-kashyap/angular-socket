587a333376b21469610c4aee21ea038416294a01
Anand Kashyap <anandkashyap60@gmail.com>
2020-03-19 18:27:07 +0530

    env fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   hmr: false,
-  socketUrl: 'https://node-socket-ak.herokuapp.com',
-  baseUrl: 'https://node-socket-ak.herokuapp.com',
+  socketUrl: 'https://node-socket-pwa.herokuapp.com',
+  baseUrl: 'https://angular-socket.back4app.io',
   vapid: {
     publicKey: 'BMQ1_Pf4rKfR668iDD4trSMujq1hTBX4GhAkPhZ_CvWaZDY_ImMmMQ159g60w6XmbMt1X56MueyKxto3Kj9n1NU',
     privateKey: 'PGM0h7yUdmijC1DUHGFCGvxfFB9zeRf7TPdJT7DC5zc'
