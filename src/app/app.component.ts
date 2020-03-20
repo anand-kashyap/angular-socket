a8fdb56db6439c77f2df6492a6f73aa70296cb6c
Anand Kashyap <anandkashyap60@gmail.com>
2020-03-20 18:21:34 +0530

    fixed disconnect on logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { setTheme } from 'ngx-bootstrap/utils';
 import { routerTransition } from './animations/routerTransition';
 import { Router } from '@angular/router';
 import { SocketService } from './user/socket.service';
 import { Subscription } from 'rxjs';
+import { environment } from '@env/environment';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,7 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./app.component.scss'],
   animations: [routerTransition]
 })
-export class AppComponent implements OnInit, OnDestroy {
-  pullrefHeight: number;
-  pullTxt: string;
+export class AppComponent implements OnInit {
   noPull = false;
   connected: Subscription;
   constructor(private router: Router, private sService: SocketService) {
@@ -22,25 +21,29 @@ export class AppComponent implements OnInit, OnDestroy {
 
   // todo disconnect active socket on logout
   ngOnInit() {
+    this.showConfig();
     this.sService.loggedIn$.subscribe(isActive => {
       if (isActive && !this.connected) {
         this.connected = this.sService.connectSocket().subscribe(onli => console.log(onli));
         console.log('set active');
+      } else {
+        this.connected.unsubscribe();
+        this.connected = null;
+        console.log('remove active');
       }
     });
     if (this.sService.isLoggedIn()) {
       this.sService.loggedIn$.next(true);
     }
     this.router.events.subscribe(res => {
-      // console.log(this.router.url, "Current URL");
       this.noPull = this.router.url.startsWith('/user/chat');
     });
   }
 
-  ngOnDestroy() {
-    console.log('remove active');
-    // tslint:disable-next-line: no-unused-expression
-    this.connected && this.connected.unsubscribe();
+  private showConfig() {
+    const css = 'color:#007bff;font-size:0.85rem;font-weight:bold;';
+    console.log('%cMaking api calls to: ', css, environment.socketUrl);
+    console.log(`%cCurrent app url is: `, css + 'color:orange;', environment.baseUrl);
   }
 
   getState(outlet) {
@@ -50,28 +53,5 @@ export class AppComponent implements OnInit, OnDestroy {
   onPull(r) {
     // console.log(r);
     r.activated.instance.ngOnInit();
-  }
-
-  pulling(height) {
-    if (height > 60) {
-      this.pullTxt = 'leave to refresh';
-    } else {
-      this.pullTxt = 'pull to refresh';
-    }
-    this.pullrefHeight = height;
-  }
-
-  pullComp(success: boolean) {
-    console.log('comple', success);
-
-    if (success) {
-      this.pullTxt = 'refreshing!';
-      setTimeout(() => {
-        this.pullrefHeight = 0;
-      }, 2000);
-    } else {
-      this.pullTxt = 'pull to refresh';
-      this.pullrefHeight = 0;
-    }
   }
 }
