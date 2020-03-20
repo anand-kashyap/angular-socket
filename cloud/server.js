9aa1cff51bc8a9eacafd30f718c9bf68a3198a0b
Anand Kashyap <anandkashyap60@gmail.com>
2020-03-20 15:17:52 +0530

    redirect partial fix; add todo

--- cloud/server.js
+++ cloud/server.js
@@ -1,26 +1,14 @@
 //Install express server
-express = require('express'),
-  compression = require('compression'),
-  // enforce = require('express-sslify'),
-  path = require('path');
+const express = require('express'),
+  // compression = require('compression'),
+  path = require('path'),
+  redirect = require('parse-express-https-redirect');
 
 const app = express();
-// app.use(enforce.HTTPS({ trustProtoHeader: true }));
-app.use(compression());
+// app.use(compression());
 // Serve only the static files form the dist director
 app.use(express.static('../public'));
-
-app.use(function () {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-});
-
+app.use(redirect());
 app.get('/*', function (req, res) {
 
   res.sendFile(path.join(__dirname, '../public/index.html'));
