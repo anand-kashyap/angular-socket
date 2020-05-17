ca94b529995171bff20affa836f88fe61d9dcb1c
Anand Kashyap <anandkashyap60@gmail.com>
2020-05-17 11:13:11 +0530

    aws config added

---
+++ src/environments/environment.aws.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  hmr: false,
+  socketUrl: 'https://ng-socket.jsclub.dev',
+  baseUrl: 'https://ng-socket.jsclub.dev',
+  vapid: {
+    publicKey: 'BMQ1_Pf4rKfR668iDD4trSMujq1hTBX4GhAkPhZ_CvWaZDY_ImMmMQ159g60w6XmbMt1X56MueyKxto3Kj9n1NU',
+    privateKey: 'PGM0h7yUdmijC1DUHGFCGvxfFB9zeRf7TPdJT7DC5zc'
+  }
+};
