39b0f31018e4444cfe5fde512ca3b00b38fb6089
Anand Kashyap <anandkashyap60@gmail.com>
2020-05-18 19:39:15 +0530

    aws nginx setup

--- src/environments/environment.aws.ts
+++ src/environments/environment.aws.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   hmr: false,
-  socketUrl: 'https://ng-socket.jsclub.dev',
+  socketUrl: 'https://ng-socket.jsclub.dev/api',
   baseUrl: 'https://ng-socket.jsclub.dev',
   vapid: {
     publicKey: 'BMQ1_Pf4rKfR668iDD4trSMujq1hTBX4GhAkPhZ_CvWaZDY_ImMmMQ159g60w6XmbMt1X56MueyKxto3Kj9n1NU',
